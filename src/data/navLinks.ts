2da372fcc720f28072b32e7f93135fd89d59afd0
mideyolu <olumideoluwuyi91@gmail.com>
2025-09-11 00:11:10 +0100

    Included my webpage section links

---
+++ src/data/navLinks.ts
@@ -0,0 +1,10 @@
+// navLinks.ts
+import type { NavLink } from "@/types/navbar";
+
+export const navLinks: NavLink[] = [
+    { name: "Home", href: "#home" },
+    { name: "About", href: "#about" },
+    { name: "Projects", href: "#projects" },
+    { name: "Skills", href: "#skills" },
+    { name: "Contact", href: "#contact" },
+];
