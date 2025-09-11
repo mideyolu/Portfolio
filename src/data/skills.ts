41a8c912d18e2d954949d21c823f8b5f0ae93bec
mideyolu <olumideoluwuyi91@gmail.com>
2025-09-11 14:37:25 +0100

    included my skills types and data

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,55 +1,54 @@
-// // skillinfo.ts
-// import type { Skill } from "@/types/skills";
+// skillinfo.ts
+import type { Skill } from "@/types/skills";
 
-// // Tech skills data
-// export const skills: Skill[] = [
-//     {
-//         name: "Python",
-//         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
-//     },
-//     {
-//         name: "Excel",
-//         icon: "https://icons.veryicon.com/png/o/miscellaneous/logo-design-of-lingzhuyun/icon-file-type-excel.png",
-//     },
-//     {
-//         name: "PowerBI",
-//         icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/New_Power_BI_Logo.svg/1200px-New_Power_BI_Logo.svg.png",
-//     },
-//     ,
-//     {
-//         name: "React",
-//         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
-//     },
-//     {
-//         name: "Firebase",
-//         icon: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
-//     },
-//     {
-//         name: "TypeScript",
-//         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-//     },
-//     {
-//         name: "Tailwind CSS",
-//         icon: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
-//     },
-//     {
-//         name: "MySQL",
-//         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
-//     },
-//     {
-//         name: "PostgreSQL",
-//         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
-//     },
-//     {
-//         name: "TensorFlow",
-//         icon: "https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg",
-//     },
-//     {
-//         name: "FastAPI",
-//         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original.svg",
-//     },
-//     {
-//         name: "Git",
-//         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
-//     },
-// ];
+// Tech skills data
+export const skills: Skill[] = [
+    {
+        name: "Python",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+    },
+    {
+        name: "Excel",
+        icon: "https://icons.veryicon.com/png/o/miscellaneous/logo-design-of-lingzhuyun/icon-file-type-excel.png",
+    },
+    {
+        name: "PowerBI",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/New_Power_BI_Logo.svg/1200px-New_Power_BI_Logo.svg.png",
+    },
+    {
+        name: "React",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    },
+    {
+        name: "Firebase",
+        icon: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+    },
+    {
+        name: "TypeScript",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+    },
+    {
+        name: "Tailwind CSS",
+        icon: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+    },
+    {
+        name: "MySQL",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+    },
+    {
+        name: "PostgreSQL",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+    },
+    {
+        name: "TensorFlow",
+        icon: "https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg",
+    },
+    {
+        name: "FastAPI",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original.svg",
+    },
+    {
+        name: "Git",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+    },
+];
