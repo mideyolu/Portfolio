df1aade578d05c244456c85b6094bd0cb8db1c0a
mideyolu <olumideoluwuyi91@gmail.com>
2025-09-11 10:18:28 +0100

    Included updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import About from "@/components/About/About";
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar/Navbar";
-import Skills from "@/components/Skill/Skills";
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
-import Projects from "./components/Projects/Projects";
-import "./index.css";
-import {Toaster} from "react-hot-toast"
+// import Skills from "@/components/Skill/Skills";
 import { useRef } from "react";
-
+import { Toaster } from "react-hot-toast";
+// import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+// import Projects from "./components/Projects/Projects";
+import "./index.css";
 
 const App = () => {
-    const contactRef = useRef<HTMLDivElement | null>(null)
+    const contactRef = useRef<HTMLDivElement | null>(null);
 
     return (
         <div className="">
@@ -19,9 +18,9 @@ const App = () => {
             <Navbar contactRef={contactRef} />
             <Hero />
             <About />
-            <Projects />
+            {/* <Projects />
             <Skills />
-            <Contact ref={contactRef} />
+            <Contact ref={contactRef} /> */}
             <Footer />
         </div>
     );
