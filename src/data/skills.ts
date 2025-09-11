d365a7d97e317b8ee3f3f37b5fe05037951bba37
mideyolu <olumideoluwuyi91@gmail.com>
2025-09-11 10:30:08 +0100

    Included updates

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -3,72 +3,53 @@
 
 // // Tech skills data
 // export const skills: Skill[] = [
-//     { name: "Python", icon: "/assets/tech/python.png" },
-//     { name: "Excel", icon: "/assets/tech/excel.png" },
-//     { name: "PowerBI", icon: "/assets/tech/powerbi.png" },
-//     { name: "React", icon: "/assets/tech/react.png" },
-//     { name: "Tailwind CSS", icon: "/assets/tech/tailwindcss.png" },
-//     { name: "MySQL", icon: "/assets/tech/mysql.png" },
-//     { name: "PostgreSQL", icon: "/assets/tech/postgre.png" },
-//     { name: "TensorFlow", icon: "/assets/tech/tensorflow.png" },
-//     { name: "FastAPI", icon: "/assets/tech/FastAPI.png" },
-//     { name: "Git", icon: "/assets/tech/git.png" },
-//     { name: "Firebase", icon: "/assets/tech/firebase.png" },
+//     {
+//         name: "Python",
+//         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+//     },
+//     {
+//         name: "Excel",
+//         icon: "https://icons.veryicon.com/png/o/miscellaneous/logo-design-of-lingzhuyun/icon-file-type-excel.png",
+//     },
+//     {
+//         name: "PowerBI",
+//         icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/New_Power_BI_Logo.svg/1200px-New_Power_BI_Logo.svg.png",
+//     },
+//     ,
+//     {
+//         name: "React",
+//         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+//     },
+//     {
+//         name: "Firebase",
+//         icon: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+//     },
+//     {
+//         name: "TypeScript",
+//         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+//     },
+//     {
+//         name: "Tailwind CSS",
+//         icon: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+//     },
+//     {
+//         name: "MySQL",
+//         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+//     },
+//     {
+//         name: "PostgreSQL",
+//         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+//     },
+//     {
+//         name: "TensorFlow",
+//         icon: "https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg",
+//     },
+//     {
+//         name: "FastAPI",
+//         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original.svg",
+//     },
+//     {
+//         name: "Git",
+//         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+//     },
 // ];
-
-
-// skillinfo.ts
-import type { Skill } from "@/types/skills";
-
-// Tech skills data
-export const skills: Skill[] = [
-    {
-        name: "Python",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
-    },
-    {
-        name: "Excel",
-        icon: "https://icons.veryicon.com/png/o/miscellaneous/logo-design-of-lingzhuyun/icon-file-type-excel.png",
-    },
-    {
-        name: "PowerBI",
-        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/New_Power_BI_Logo.svg/1200px-New_Power_BI_Logo.svg.png",
-    },
-    ,
-    {
-        name: "React",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
-    },
-    {
-        name: "Firebase",
-        icon: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
-    },
-    {
-        name: "TypeScript",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-    },
-    {
-        name: "Tailwind CSS",
-        icon: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
-    },
-    {
-        name: "MySQL",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
-    },
-    {
-        name: "PostgreSQL",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
-    },
-    {
-        name: "TensorFlow",
-        icon: "https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg",
-    },
-    {
-        name: "FastAPI",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original.svg",
-    },
-    {
-        name: "Git",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
-    },
-];
