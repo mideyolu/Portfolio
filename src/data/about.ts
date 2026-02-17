7424ac6c3073f04dd1b2f3c876cd8626b4a42035
Oluwuyi Olumide <137781836+mideyolu@users.noreply.github.com>
2026-02-17 11:11:51 +0100

    Update about.ts

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,37 +1,4 @@
 
-
-// export const aboutItems = [
-//     {
-//         title: "About Me",
-//         description: "I'm a Software Engineer specializing in AI, Machine Learning, and Data Science. I graduated with a First Class Honors in Software Engineering from Babcock University, with strong foundations in algorithms, databases, and software design.",
-
-//         tags: [
-//             "AI & ML",
-//             "Data Science",
-//             "EDA",
-//             "Data Analysis",
-//             "Visualization",
-//             "Web Development",
-//         ],
-//         animation: slideFromUp,
-//     },
-//     {
-//         title: "Experience & Volunteering",
-//         description:
-//             "I have over a year of experience in data analytics, AI-driven solutions, and frontend development. At the National Center for Artificial Intelligence and Robotics, I worked on an AI-powered attendance system and a smart IoT parking solutions. I interned at Bizmarrow Technologies, creating responsive web interfaces, and volunteered as Resource Manager at GDG Babcock (Data/AI), where I organized workshops and supported open-source initiatives.",
-
-
-//         tags: [
-//             "Internship",
-//             "Projects",
-//             "IoT Applications",
-//             "Community Engagement",
-//         ],
-//         animation: slideFromRight,
-//     },
-// ];
-
-
 // about.ts
 
 
@@ -58,7 +25,7 @@ export const aboutItems = [
         ],
         animation: slideFromUp,
     },
-   /* {
+    {
         title: "NCAIR",
         description:
             "At the National Center for Artificial Intelligence and Robotics, I worked on cutting-edge projects including an AI-powered attendance system and smart IoT parking solutions. This experience allowed me to apply my knowledge in artificial intelligence to solve real-world problems and develop innovative technological solutions.",
@@ -69,7 +36,7 @@ export const aboutItems = [
             "Project Development",
         ],
         animation: slideFromRight,
-   },*/
+   },
     {
         title: "Bizmarrow Technologies",
         description:
